9eda4f6c79702b60ca2a97020ab1da3aaf4d5295
Joey Rivera <josephrivera5000@gmail.com>
2021-07-08 12:15:03 -0700

    Refactored project to use webpack and avoid port sharing issues

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  devtool: 'source-map',
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.(js|jsx)$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react'],
+        }
+      },
+      {
+        test: /\.(gif|svg|jpg|png)$/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader'}
+        ]
+      }
+    ]
+  }
+};
